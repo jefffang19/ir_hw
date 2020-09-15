789c424229bcc59e09343148db5591c237b622ac
jefffang19 <jefffang07@gmail.com>
2020-09-15 15:53:29 +0800

    Create Model Article, Word

--- hw1/hw_server/search_engine/models.py
+++ hw1/hw_server/search_engine/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    abstract = models.CharField(max_length=200)
+
+class Word(models.Model):
+    context = models.CharField(max_length=200)
+    position = models.ManyToManyField(Article)
+    
