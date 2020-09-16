0dc0b04036a2146f461e71f2b943d859316b01b1
jefffang19 <jefffang07@gmail.com>
2020-09-16 10:31:32 +0800

    Create a controller to show all articles

--- hw1/hw_server/search_engine/urls.py
+++ hw1/hw_server/search_engine/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from django.views.generic import TemplateView
 from . import views
+
 
 urlpatterns = [
     path('import_json', views.import_json, name = 'import_json'),
+    path('show_articles', views.show_articles, name = 'show_articles'),
 ]
--- hw1/hw_server/search_engine/views.py
+++ hw1/hw_server/search_engine/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Article, Word
 from django.http import HttpResponse, JsonResponse
+from django.template import loader
 
 from search_engine.parsing_utils import data_processor
 
@@ -18,3 +19,8 @@ def import_json(request):
             w.position.add(a)
 
     return JsonResponse({"Import file" : "Json", "Status" : "Success"})
+
+def show_articles(request):
+    all_articles = Article.objects.all()
+    
+    return HttpResponse(loader.get_template('search_engine/show_articles.html').render({ 'articles' : [i.abstract for i in all_articles]} , request))
