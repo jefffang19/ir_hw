8cca85f6ee4e953f6db11bdbc2ded62c8554cf0e
jefffang19 <jefffang07@gmail.com>
2020-09-18 13:57:56 +0800

    Add a search page template

--- hw1/hw_server/search_engine/urls.py
+++ hw1/hw_server/search_engine/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path('import_json', views.import_json, name = 'import_json'),
     path('show_articles', views.show_articles, name = 'show_articles'),
+    path('search_page', TemplateView.as_view(template_name = 'search_engine/search_page.html') ),
 ]
