140328a9981420fd016bb20b703719ccc76c641b
jefffang19 <jefffang07@gmail.com>
2020-10-09 16:48:20 +0800

    Add import pubmed csv's api

---
+++ hw2/hw_server/search_engine/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    abstract = models.CharField(max_length=200)
+    
+class Word(models.Model):
+    context = models.CharField(max_length=200)
+    position = models.ManyToManyField(Article)
+    pos_in_a_article = models.IntegerField()
+
+
+    
---
+++ hw2/hw_server/search_engine/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from django.views.generic import TemplateView
+from . import views
+
+
+urlpatterns = [
+    path('import_json', views.import_json, name = 'import_json'),
+    path('import_xml', views.import_xml, name = 'import_xml'),
+    path('show_articles', views.show_articles, name = 'show_articles'),
+    # path('search_page', TemplateView.as_view(template_name = 'search_engine/search_page.html') ),
+    path('search_page', views.get_keywords, name = 'search_page'),
+    path('upload_file', views.upload_file, name = 'upload_file'),
+    path('zipf',views.zipf, name = 'zipf'),
+    path('import_pubmed', views.import_pubmed, name = 'import_pubmed'),
+]
