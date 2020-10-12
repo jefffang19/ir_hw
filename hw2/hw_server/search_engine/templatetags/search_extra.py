e302245035f28bc229f3fb4c60e80a3dae999690
jefffang19 <jefffang07@gmail.com>
2020-10-12 18:06:12 +0800

    Hide empty table in zipf home

--- hw2/hw_server/search_engine/templatetags/search_extra.py
+++ hw2/hw_server/search_engine/templatetags/search_extra.py
@@ -10,6 +10,10 @@ def get_element(value, key):
 def remove_period(value):
     return value[:-1]
 
+def check_empty(value):
+    return len(value) == 0
+
 
 register.filter('get_element', get_element)
 register.filter('remove_period', remove_period)
+register.filter('check_empty', check_empty)
