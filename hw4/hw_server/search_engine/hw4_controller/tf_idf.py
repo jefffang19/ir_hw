00321e0d872c17919e1621b9a4e8a984411cad58
jefffang19 <jefffang07@gmail.com>
2020-11-16 09:27:09 +0800

    Add hw4 tfidf template

--- hw4/hw_server/search_engine/hw4_controller/tf_idf.py
+++ hw4/hw_server/search_engine/hw4_controller/tf_idf.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from ..models import Article
+from ..forms import WordForm
 
 from .tfidf_algorithm import tfidf
 import numpy as np  # array handling
@@ -12,4 +13,9 @@ def show_tfidef(request):
 
     _tfidf = tfidf('COVID-19', 1, 1)
     _tfidf.sort(key=lambda x: x[1], reverse=True)
-    return HttpResponse([_tfidf])
+    # return HttpResponse([_tfidf])
+
+    form = WordForm()
+
+    template_dict = {'form' : form}
+    return render(request, "search_engine/tfidf.html", template_dict)
