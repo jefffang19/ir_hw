ffff278e03c1a7317dad4b0ffd9e6cb629abcd4a
jefffang19 <jefffang07@gmail.com>
2020-11-16 16:37:12 +0800

    Change input keyword from wordForm to ajax

--- hw4/hw_server/search_engine/hw4_controller/tf_idf.py
+++ hw4/hw_server/search_engine/hw4_controller/tf_idf.py
@@ -8,48 +8,43 @@ import numpy as np  # array handling
 
 from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def show_tfidef(request):
     # user search
     if request.method == 'POST':
-        form = WordForm(request.POST)
         # get search word
-        if form.is_valid():
-            # we do NOT stem in this hw
-            origin_keyword = form.cleaned_data['keywords']
+        # we do NOT stem in this hw
+        origin_keyword = request.POST['keyword']
+        print(origin_keyword)
 
-            a = Article.objects.all()
+        a = Article.objects.all()
 
-            _tfidf = tfidf(origin_keyword, 0, 1)
-            _tfidf.sort(key=lambda x: x[1], reverse=True)
+        _tfidf = tfidf(origin_keyword, 0, 1)
+        _tfidf.sort(key=lambda x: x[1], reverse=True)
 
-            # prepare data for template
-            docs_ranking = [i[0] for i in _tfidf]
-            docs_weight = [i[1] for i in _tfidf]
+        # prepare data for template
+        docs_ranking = [i[0] for i in _tfidf]
+        docs_weight = [i[1] for i in _tfidf]
 
-            # display formula images
-            available_formula = ['images/tf_raw_count.png', 'images/tf_term_frequency.png',
-                                 'images/tf_log_normalization.png',
-                                 'images/idf_inverse_document_frequency.png', 'images/idf_unary.png',
-                                 'images/idf_inverse_document_frequency_smooth.png']
-
-            template_dict = {'form': form, 'l_titles': docs_ranking, 'l_weights': docs_weight,
-                             'r_titles': docs_ranking, 'r_weights': docs_weight,
-                             'formula': available_formula}
-            return render(request, "search_engine/tfidf.html", template_dict)
-
-        else:
-            return HttpResponse('keyword serach failed')
-
-    elif request.method == 'GET':
-        form = WordForm()
-
+        # display formula images
         available_formula = ['images/tf_raw_count.png', 'images/tf_term_frequency.png',
                              'images/tf_log_normalization.png',
                              'images/idf_inverse_document_frequency.png', 'images/idf_unary.png',
                              'images/idf_inverse_document_frequency_smooth.png']
 
-        template_dict = {'form': form, 'formula': available_formula}
+        template_dict = {'l_titles': docs_ranking, 'l_weights': docs_weight,
+                         'r_titles': docs_ranking, 'r_weights': docs_weight,
+                         'formula': available_formula}
+        # return render(request, "search_engine/tfidf.html", template_dict)
+        return JsonResponse(template_dict, safe=False)
+
+    elif request.method == 'GET':
+        available_formula = ['images/tf_raw_count.png', 'images/tf_term_frequency.png',
+                             'images/tf_log_normalization.png',
+                             'images/idf_inverse_document_frequency.png', 'images/idf_unary.png',
+                             'images/idf_inverse_document_frequency_smooth.png']
+
+        template_dict = {'formula': available_formula}
         return render(request, "search_engine/tfidf.html", template_dict)
 
     else:
