43c96913fed539d5127ac7c444b42aa69883cf5d
jefffang19 <jefffang07@gmail.com>
2020-11-16 09:45:22 +0800

    Add show documents title on template

--- hw4/hw_server/search_engine/hw4_controller/tf_idf.py
+++ hw4/hw_server/search_engine/hw4_controller/tf_idf.py
@@ -8,14 +8,19 @@ import numpy as np  # array handling
 
 from django.views.decorators.csrf import csrf_exempt
 
+
 def show_tfidef(request):
+    form = WordForm()
+
     a = Article.objects.all()
 
     _tfidf = tfidf('COVID-19', 1, 1)
     _tfidf.sort(key=lambda x: x[1], reverse=True)
-    # return HttpResponse([_tfidf])
 
-    form = WordForm()
+    # prepare data for template
+    docs_ranking = [i[0] for i in _tfidf]
+    docs_weight = [i[1] for i in _tfidf]
 
-    template_dict = {'form' : form}
+    template_dict = {'form': form, 'l_titles': docs_ranking, 'l_weights': docs_weight,
+                     'r_titles': docs_ranking, 'r_weights': docs_weight}
     return render(request, "search_engine/tfidf.html", template_dict)
